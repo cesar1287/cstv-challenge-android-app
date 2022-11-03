b31b28215c0f812372db0bdd0366a0ca4146fe33
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2022-11-03 09:22:30 -0300

    initial setup for navigation and hilt

--- app/src/main/java/com/github/cesar1287/challengecstv/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/challengecstv/MainActivity.kt
@@ -2,7 +2,9 @@ package com.github.cesar1287.challengecstv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
