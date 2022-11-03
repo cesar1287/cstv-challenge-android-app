0e1f7ad312053a00252056da4bab71c360cb4108
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2022-11-03 20:32:06 -0300

    code improvent

--- app/src/main/java/com/github/cesar1287/challengecstv/features/home/presentation/HomeFragment.kt
+++ app/src/main/java/com/github/cesar1287/challengecstv/features/home/presentation/HomeFragment.kt
@@ -68,7 +68,7 @@ class HomeFragment : Fragment() {
             }
 
             it.btHomeTryAgain.setOnClickListener {
-                homeAdapter.refresh()
+                homeAdapter.retry()
             }
         }
     }
