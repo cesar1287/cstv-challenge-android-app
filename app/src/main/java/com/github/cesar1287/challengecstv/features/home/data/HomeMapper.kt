87180949d1c10cbbf50d918d35591b7165f9b591
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2022-11-04 11:28:35 -0300

    adjusted tooltip with correct data

--- app/src/main/java/com/github/cesar1287/challengecstv/features/home/data/HomeMapper.kt
+++ app/src/main/java/com/github/cesar1287/challengecstv/features/home/data/HomeMapper.kt
@@ -1,7 +1,12 @@
 package com.github.cesar1287.challengecstv.features.home.data
 
 import com.github.cesar1287.challengecstv.model.Match
+import com.github.cesar1287.challengecstv.model.MatchStatus
 import com.github.cesar1287.challengecstv.model.MatchVO
+import com.github.cesar1287.challengecstv.utils.PandaScoreApi.STATUS_FINISHED
+import com.github.cesar1287.challengecstv.utils.PandaScoreApi.STATUS_NOT_PLAYED
+import com.github.cesar1287.challengecstv.utils.PandaScoreApi.STATUS_NOT_STARTED
+import com.github.cesar1287.challengecstv.utils.PandaScoreApi.STATUS_RUNNING
 import javax.inject.Inject
 
 interface HomeMapper {
@@ -22,8 +27,18 @@ class HomeMapperImpl @Inject constructor(): HomeMapper {
             teamBImageUrl = teamB.imageUrl,
             leagueName = match.league.name,
             leagueImageUrl = match.league.imageUrl,
-            seriesName = match.serie.fullName
+            seriesName = match.serie.fullName,
+            matchStatus = getMatchStatus(match.status),
+            datePretty = "Ter, 21:00"
         )
     }
+
+    private fun getMatchStatus(status: String) =
+        when(status) {
+            STATUS_FINISHED,
+            STATUS_NOT_PLAYED -> MatchStatus.FINISHED
+            STATUS_RUNNING -> MatchStatus.RUNNING
+            else -> MatchStatus.NOT_STARTED
+        }
 
 }
